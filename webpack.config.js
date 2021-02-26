c1a2af47a60b1a1a519eb21cc6052bcae245bc80
qxl <1434611240@qq.com>
2021-02-26 11:52:58 +0800

    提交webpack初次使用

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
-const path = require("path")
+const path = require("path");
 
 module.exports = {
   entry: path.join(__dirname, './page/index.html'),
   output:{
-    filename: 'king'
+    filename: 'king.js'
   },
   module:{
     rules: [
       {
         test: /\.html$/,
-        use: '',
+        use: "html-loader",
       },
     ]
   }
